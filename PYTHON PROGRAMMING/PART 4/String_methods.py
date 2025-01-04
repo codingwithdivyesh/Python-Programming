46f78ba3a713561c0c0b9e8c6378e3865fc454ab
divyesh coder <divyeshmodhvadiya761@gmail.com>
2025-01-04 19:05:06 +0530

    Add files via upload

---
+++ PYTHON PROGRAMMING/PART 4/String_methods.py
@@ -0,0 +1,22 @@
+#int() #string
+#str.lower() #methods 
+
+#* No changed original string , if you want to do you have to make a verbal 👇
+
+#! new = print(text.upper)
+#! print(new)
+
+text = "divyesh is my name" 
+
+#? all the methods of string 👇
+
+print(text.lower()) #lower case all the string content 
+print(text.capitalize()) #capital first word letter 
+print(text.title()) #this will capital only a word first character
+print(text.strip()) #cut the all space 
+print(text.lstrip()) # only cut left side space 
+print(text.rstrip()) # cut right space only 
+print(text.find("my")) # finding 
+print(text.replace("divyesh"," coder")) #this replace my word to other word
+print("divyesh" in text ) #print true 
+print("codingwithdivyesh" in text ) #print false 
