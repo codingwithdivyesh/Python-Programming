1bac02f111cf79d84139fd1892735b6da6f0a716
divyesh coder <divyeshmodhvadiya761@gmail.com>
2025-01-04 19:25:05 +0530

    Add files via upload

---
+++ PYTHON PROGRAMMING/PART 5/Numbers.py
@@ -0,0 +1,26 @@
+number1 = 4 #? integer
+
+number2 = 1.5 #? float
+
+number3 = 2 
+
+
+result = number1 + number2 # plus  number 
+result = number1 - number2 # minis  number 
+result = number1 * number2 # multiply number
+result = number1 / number2 # divide  number #! float 
+result = number1 // number3 # divide2 number #! integer
+result = number1 ** number3 # power of  number 
+result = number1 % number3 # reminder of  number 
+
+
+print (result)
+
+#* shortcut trick for plus and minis etc..
+
+examples = number1 + 12 #// non-shortcut
+number1 += 12 #? with shortcut trick 
+
+
+print(examples)
+print(number1)
