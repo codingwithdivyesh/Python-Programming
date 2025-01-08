5d64f76ca9a365b32e7e2b44b7b50a14c8080499
divyesh coder <divyeshmodhvadiya761@gmail.com>
2025-01-08 21:28:21 +0530

    Add files via upload

---
+++ PYTHON PROGRAMMING/PART 9/for_loop.py
@@ -0,0 +1,65 @@
+
+
+#?1
+
+# for age in range(5):
+
+#      print( f" my number is \" {age} \" " , "this is called number")
+
+
+# print(" this is the end")
+
+
+#?2
+
+# for number in range(5,10):
+
+   
+#      print( number , "this is called number")
+
+
+# print(" this is the end")
+
+
+#?3
+
+
+
+# for number in range(5,30,5):
+
+   
+#     print( number , "this is called number")
+
+
+# print(" this is the end")
+
+
+
+#?4
+
+
+  
+# for number in range(5,20):
+#     if number % 2 == 0:
+#         print(number)
+
+
+
+
+# print("this is the end")
+
+
+#?5
+
+
+# for table in range(1,11):
+#     print(f" 2 x {table} = { table* 2 } ")
+
+#?6 
+
+# name = "divyesh"
+
+# for chr in name:
+#     print(chr)
+
+
